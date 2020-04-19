3c6fae754707f13a06f66376b0c40991d98f8e2e
adorn4711 <adorn4711@gmail.com>
2020-04-19 21:21:35 +0200

    Simple unit test with pytest
    call pytest in folder unittest

---
+++ unittest/test_simple_function.py
@@ -0,0 +1,11 @@
+from simple_function import add
+import pytest
+
+
+@pytest.mark.parametrize('x, y, result', [
+    (10, 10, 20),
+    (5, 5, 10),
+    (6, 6, 12)
+])
+def test_add(x, y, result):
+    assert add(x, y) == result
