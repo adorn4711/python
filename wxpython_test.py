03eb30b08f1b33345e62d63158181a967c1b2280
adorn4711 <adorn4711@gmail.com>
2020-04-25 12:43:16 +0000

    remove #usr/bin

--- wxpython_test.py
+++ wxpython_test.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
 #import wxversion
 #wxversion.select("2.8")
 #import wx, wx.html
 #import sys
-import wx
+import wxPython as wx
 
 app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
 frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
