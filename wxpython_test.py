380ae1fdc31e0582eb1fd4b7b50e487a61fafb07
adorn4711 <adorn4711@gmail.com>
2020-04-25 12:06:42 +0000

    wxpython part2

---
+++ wxpython_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#import wxversion
+#wxversion.select("2.8")
+#import wx, wx.html
+#import sys
+import wx
+
+app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
+frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
+frame.Show(True)     # Show the frame.
+app.MainLoop()
